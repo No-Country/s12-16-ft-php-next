b0e803e8706432b7159c3e0f1e811c50d793d815
Vladimir Infante <infantevladimir@gmail.com>
2023-11-23 18:20:14 -0500

    feat: added nextjs client config

---
+++ client/src/components/ui/Text/styles.js
@@ -0,0 +1,13 @@
+import { cva } from 'class-variance-authority';
+
+export const textVariants = cva('', {
+  variants: {
+    variant: {
+      base: 'text-base font-normal',
+      title: 'text-xl font-bold text-red-500',
+    },
+  },
+  defaultVariants: {
+    variant: 'body',
+  },
+});
