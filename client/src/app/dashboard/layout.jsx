e1c607ddbda3ec83542729279082387412fc357c
Diego <redgo110.krextro@gmail.com>
2023-11-30 14:50:25 -0300

    feat: show and hide sidenav

--- client/src/app/dashboard/layout.jsx
+++ client/src/app/dashboard/layout.jsx
@@ -1,23 +1,30 @@
+"use client"
 import { Inter } from "next/font/google";
 import { Toaster } from "sonner";
 import SideNav from "@/components/Navbar/SideNav";
-
+import React, { useState } from "react";
 const inter = Inter({ subsets: ["latin"] });
 
-export const metadata = {
-    title: "Sistema de Gestión de Inventario",
-    description: "Sistema de Gestión de Inventario hecho por No-Country",
-};
+// export const metadata = {
+//     title: "Sistema de Gestión de Inventario",
+//     description: "Sistema de Gestión de Inventario hecho por No-Country",
+// };
 
 export default function Layout({ children }) {
+    const [isvisible, setIsvisible] = useState(true);
+
+    const toggleSideNav = () => {
+        setIsvisible(!isvisible);
+    }
     return (
         <html lang="es">
             <body className={inter.className}>
                 <Toaster position="top-center" richColors />
                 <div className="flex h-screen flex-col md:flex-row md:overflow-hidden">
-                    <div className="w-full flex-none md:w-64">
-                        <SideNav />
+                    <div className={`${isvisible ? ' w-full flex-none md:w-64' : 'hidden'}`}>
+                        <SideNav toggleSideNav={toggleSideNav} />
                     </div>
+                    <button onClick={toggleSideNav}>abrir</button>
                     <div className="flex-grow p-6 md:overflow-y-auto md:p-12">{children}</div>
                 </div>
             </body>
--- client/src/components/Navbar/SideNav.jsx
+++ client/src/components/Navbar/SideNav.jsx
@@ -2,11 +2,14 @@ import Image from "next/image";
 import NavBar from "../ui/NavLinks/nav-links";
 import { ArrowLeftOnRectangleIcon } from '@heroicons/react/24/outline'
 
-export default function SideNav() {
+export default function SideNav(props) {
+
+    // const { toggleSideNav } = props.toggleSideNav
+
     return (
         <div className="flex h-full flex-col px-3 py-4 md:px-2 bg-bg-custom">
             <div className="flex justify-center p-10" >
-                <ArrowLeftOnRectangleIcon className="h-10 w-10 text-txt-custom" />
+                <ArrowLeftOnRectangleIcon onClick={props.toggleSideNav} className="h-10 w-10 text-txt-custom" />
             </div>
             <div className="flex grow flex-row justify-between space-x-2 md:flex-col md:space-x-0 md:space-y-2">
                 <NavBar />
