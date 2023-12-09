4fff1479042d443f7f092e68ae38adc340ff58b9
Diego <redgo110.krextro@gmail.com>
2023-12-08 23:43:09 -0300

    feat: user info componente was created

--- client/src/components/Navbar/SideNav.jsx
+++ client/src/components/Navbar/SideNav.jsx
@@ -1,28 +1,22 @@
-import Image from "next/image";
 import NavBar from "../ui/NavLinks/nav-links";
 import { ArrowLeftOnRectangleIcon } from "@heroicons/react/24/outline";
+import UserInfo from "../UserInfo/UserInfo";
 
 
 export default function SideNav() {
 
   return (
-    <div className="flex h-full flex-col bg-bg-custom px-3 py-4 md:px-2">
-      <div className="flex justify-center p-10">
+    <div className="flex h-full flex-col bg-bg-custom md:px-0 rounded-b-2xl">
+      <div className="flex justify-center pt-9">
         <ArrowLeftOnRectangleIcon
           className="h-10 w-10 text-txt-custom"
         />
       </div>
-      <div className="flex grow flex-row justify-between space-x-2 md:flex-col md:space-x-0 md:space-y-2">
-        <NavBar />
-        <div className="flex justify-center bg-bg-img py-10">
-          <Image
-            className="w-24 h-24 rounded-full object-cover"
-            src="/workerman.png"
-            width={'100'}
-            height={'100'}
-            alt="Imagen"
-          />
+      <div className="flex grow flex-row justify-between px-0 space-x-2 md:flex-col md:space-x-0 md:space-y-2 rounded-b-2xl">
+        <div className="px-4">
+          <NavBar />
         </div>
+        <UserInfo />
       </div>
     </div>
   );
